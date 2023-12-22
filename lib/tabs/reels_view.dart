608b7cd98ee66df06e4379709454b8eb536be340
Delos-343 <mfachry.handoko@gmail.com>
2023-12-22 21:40:20 +0800

    ignored constructor warnings

--- lib/tabs/reels_view.dart
+++ lib/tabs/reels_view.dart
@@ -5,6 +5,8 @@ class ReelsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Center(
+      child: Text('Reels View'),
+    );
   }
 }
