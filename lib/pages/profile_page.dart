45950e5914f157bbb162971f7230a53b8bc9c6b0
Delos-343 <mfachry.handoko@gmail.com>
2023-12-23 14:39:45 +0800

    Rm SafeArea

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -59,159 +59,157 @@ class _ProfilePageState extends State<ProfilePage> {
       length: 3,
       child: Scaffold(
         appBar: AppBar(),
-        body: SafeArea(
-          child: ListView(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  // Following
-                  const Column(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      Text(
-                        '1532',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18
-                        ),
-                      ),
-                      SizedBox(height: 5),
-                      Text(
-                        'Following',
-                        style: TextStyle(
-                          color: Colors.grey,
-                        ),
-                      ),
-                    ],
-                  ),
-          
-                  // Profile Picture
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                    child: Container(
-                      height: 100,
-                      width: 100,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: Colors.blueGrey[100],
-                      ),
-                    ),
-                  ),
-          
-                  // Followers
-                  const Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        '1574',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18
-                        ),
-                      ),
-                      SizedBox(height: 5),
-                      Text(
-                        'Followers',
-                        style: TextStyle(
-                          color: Colors.grey,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-
-              const SizedBox(height: 20),
-
-              // Name
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Fachry Dwi Handoko ',
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                ],
-              ),
-
-              const SizedBox(height: 5),
-
-              // Bio
-              Text(
-                'Frontend x Mobile Developer • UI/UX Designer',
-                style: TextStyle(
-                  color: Colors.grey[600],
-                  fontWeight: FontWeight.w400
-                ),
-                textAlign: TextAlign.center,
-              ),
-
-              const SizedBox(height: 5),
-
-              // Link
-              Text(
-                'Connect with Me',
-                style: TextStyle(
-                  color: Colors.blue[500],
-                  fontWeight: FontWeight.bold
-                ),
-                textAlign: TextAlign.center,
-              ),
-
-              const SizedBox(height: 20),
-
-              // Buttons
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                child: Row(
+        body: ListView(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // Following
+                const Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
                   children: [
-                    Expanded(
-                      child: Container(
-                        padding: const EdgeInsets.all(10),
-                        decoration: BoxDecoration(
-                          color: Colors.grey[400],
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                        child: const Center(
-                          child: Text('Edit Profile',),
-                        ),
+                    Text(
+                      '1532',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18
                       ),
                     ),
-                
-                    const SizedBox(width: 10),
-                
-                    Expanded(
-                      child: Container(
-                        padding: const EdgeInsets.all(10),
-                        decoration: BoxDecoration(
-                          color: Colors.black,
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                        child: const Center(
-                          child: Text(
-                            'Contact',
-                            style: TextStyle(color: Colors.white),
-                          ),
-                        ),
+                    SizedBox(height: 5),
+                    Text(
+                      'Following',
+                      style: TextStyle(
+                        color: Colors.grey,
                       ),
                     ),
                   ],
                 ),
+        
+                // Profile Picture
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  child: Container(
+                    height: 100,
+                    width: 100,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.blueGrey[100],
+                    ),
+                  ),
+                ),
+        
+                // Followers
+                const Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      '1574',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18
+                      ),
+                    ),
+                    SizedBox(height: 5),
+                    Text(
+                      'Followers',
+                      style: TextStyle(
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+        
+            const SizedBox(height: 20),
+        
+            // Name
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Fachry Dwi Handoko ',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+        
+            const SizedBox(height: 5),
+        
+            // Bio
+            Text(
+              'Frontend x Mobile Developer • UI/UX Designer',
+              style: TextStyle(
+                color: Colors.grey[600],
+                fontWeight: FontWeight.w400
               ),
-
-              const SizedBox(height: 20),
-
-              // Tab Bar
-              TabBar(
-                tabs: tabs,
+              textAlign: TextAlign.center,
+            ),
+        
+            const SizedBox(height: 5),
+        
+            // Link
+            Text(
+              'Connect with Me',
+              style: TextStyle(
+                color: Colors.blue[500],
+                fontWeight: FontWeight.bold
               ),
-              SizedBox(
-                height: 1000,
-                child: TabBarView(children: tabBarViews),
+              textAlign: TextAlign.center,
+            ),
+        
+            const SizedBox(height: 20),
+        
+            // Buttons
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      padding: const EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: Colors.grey[400],
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: const Center(
+                        child: Text('Edit Profile',),
+                      ),
+                    ),
+                  ),
+              
+                  const SizedBox(width: 10),
+              
+                  Expanded(
+                    child: Container(
+                      padding: const EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: const Center(
+                        child: Text(
+                          'Contact',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+        
+            const SizedBox(height: 20),
+        
+            // Tab Bar
+            TabBar(
+              tabs: tabs,
+            ),
+            SizedBox(
+              height: 1000,
+              child: TabBarView(children: tabBarViews),
+            ),
+          ],
         ),
       ),
     );
